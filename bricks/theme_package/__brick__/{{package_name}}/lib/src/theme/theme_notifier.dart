be1ca3f62395d7c637f16fa96f6cfa7814e41cdc
maxvyr <maxvid30@gmail.com>
2023-02-02 17:49:41 +0100

    ✨ add enum when create bricks

--- bricks/theme_package/__brick__/{{package_name}}/lib/src/theme/theme_notifier.dart
+++ bricks/theme_package/__brick__/{{package_name}}/lib/src/theme/theme_notifier.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-final themeNotifierProvider = ChangeNotifierProvider<ThemeNotifier>((_) => ThemeNotifier());
+final themeNotifierProvider =
+    ChangeNotifierProvider<ThemeNotifier>((_) => ThemeNotifier());
 
 class ThemeNotifier extends ChangeNotifier {
   ThemeNotifier() {
     _init();
   }
   ThemeMode themeMode = ThemeMode.system;
-  FlutterSecureStorage? flutterSecureStorage;
-  final key = "theme";
+  late SharedPreferences prefs;
+  final _key = "theme";
 
   Future<void> _init() async {
-    flutterSecureStorage = const FlutterSecureStorage();
-    final theme = await getTheme(key);
+    prefs = await SharedPreferences.getInstance();
+    final theme = await getTheme(_key);
     themeMode = ThemeMode.values[theme];
     notifyListeners();
   }
 
   Future<int> getTheme(String key) async {
-    return int.parse(await flutterSecureStorage?.read(key: key) ?? themeMode.index.toString());
+    return int.parse(await prefs.getString(key) ?? themeMode.index.toString());
   }
 
   Future<void> setTheme(ThemeMode mode) async {
     themeMode = mode;
     notifyListeners();
-    await flutterSecureStorage?.write(key: key, value: mode.index.toString());
+    await prefs.setString(_key, mode.index.toString());
   }
 }
