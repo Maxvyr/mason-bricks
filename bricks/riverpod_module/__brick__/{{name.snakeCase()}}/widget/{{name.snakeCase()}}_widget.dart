dfa294169f62f3d1cea9da294384d49d45246c7b
maxvyr <maxvid30@gmail.com>
2023-02-03 17:17:15 +0100

    ✨ add riverpod_module

---
+++ bricks/riverpod_module/__brick__/{{name.snakeCase()}}/widget/{{name.snakeCase()}}_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class {{name.pascalCase()}}Widget extends StatelessWidget {
+  /// TODO add your comment here
+  const {{name.pascalCase()}}Widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
