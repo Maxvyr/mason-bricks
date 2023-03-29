d4f6f45805ee0e39d28666782ff5af26e960d3fb
maxvyr <60290207+Maxvyr@users.noreply.github.com>
2023-03-29 07:31:37 +0200

    update mason riverpod_module #2

--- bricks/riverpod_module/__brick__/{{name.snakeCase()}}/repository/{{name.snakeCase()}}_repository_impl.dart
+++ bricks/riverpod_module/__brick__/{{name.snakeCase()}}/repository/{{name.snakeCase()}}_repository_impl.dart
@@ -1,8 +1,6 @@
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import '{{name.snakeCase()}}_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class {{name.pascalCase()}}Repository {
-  // TODO add your methods here
 }
 
 final {{name.camelCase()}}RepositoryProvider = Provider((ref) {
--- bricks/riverpod_module/__brick__/{{name.snakeCase()}}/view/{{name.snakeCase()}}_view.dart
+++ bricks/riverpod_module/__brick__/{{name.snakeCase()}}/view/{{name.snakeCase()}}_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class {{name.pascalCase()}}View extends ConsumerWidget {
-  /// TODO add your comment here
   const {{name.pascalCase()}}View({super.key});
 
   static const routeName = '/{{name.camelCase()}}';
